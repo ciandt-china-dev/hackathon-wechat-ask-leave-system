915092898d993903254010a37f74adac65debb66
Lily Jiang <lilyjiang@ciandt.com>
2016-07-15 13:29:39 +0800

    Rename theme

--- sites/all/themes/vacation/template.php
+++ sites/all/themes/vacation/template.php
@@ -3,13 +3,13 @@
 /**
  * Implements hook_theme().
  */
-function leave_theme($existing, $type, $theme, $path) {
+function vacation_theme($existing, $type, $theme, $path) {
   return array(
     'vocation_node_form' => array(
       'render element' => 'form',
       'template' => 'vocation-node-form',
       // this will set to module/theme path by default:
-      'path' => path_to_theme('leave') . '/templates/form',
+      'path' => path_to_theme('vacation') . '/templates/form',
     ),
   );
 }
