4a1ab2f40d9bd1c970840ed97a813f37620ff6b3
Huang jie <jieyyal@ciandt.com>
2016-07-15 22:43:52 +0800

    Change template

--- sites/all/themes/vacation/templates/form/vocation-node-form.tpl.php
+++ sites/all/themes/vacation/templates/form/vocation-node-form.tpl.php
@@ -1,9 +1,7 @@
 
-<?php //var_dump($form);?>
-
-<?php 
-	unset($form["actions"]["preview"]); 
-	print drupal_render_children($form);
-
-?>
+<?php unset($form["actions"]["preview"]);?>
+<?php hide($form["actions"]);?>
 <input name="approve_user_id" type="hidden"/>
+<?php print drupal_render_children($form);?>
+<div>eeee</div>
+<?php print render($form["actions"]);?>
