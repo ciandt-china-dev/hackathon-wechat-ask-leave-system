d9f6a18ed1dd92a013d0aacd56a4c679b7cadf28
lily_cit <lilyjiang@ciandt.com>
2017-01-06 11:46:21 +0800

    Define layout of remaining days page

--- sites/all/modules/custom/ciandt_wechat_vacation/templates/remaining-vacation-days.tpl.php
+++ sites/all/modules/custom/ciandt_wechat_vacation/templates/remaining-vacation-days.tpl.php
@@ -1,5 +1,8 @@
-<?php
-   print t("remaining vacation days: ").$variables['remaining_vacation_days']; 
-?>
-
-
+<div class="detail-header">
+	<h2><?php print t("remaining vacation days: "); ?></h2>
+	<div class="circle-item">
+	  <div class="circle">
+	   <span class="remaining-days"><?php print $variables['remaining_vacation_days']; ?></span>
+	   </div>
+	</div>
+</div>
